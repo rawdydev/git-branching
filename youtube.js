f04a77924c3959b6af059a7647ff9740327e261b
Rawdy <dadullahakbary@gmail.com>
2024-11-29 01:04:08 -0800

    maked the login responsive

--- youtube.js
+++ youtube.js
@@ -2,4 +2,6 @@ consolel.log("setuped the project.")
 console.log("added the videos")
 console.log("added the navbar.")
 console.log("added the login feature")
+console.log("maked the login responsive.")
+
 
